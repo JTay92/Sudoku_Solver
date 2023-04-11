8c1e31613a6b4213580e48801e964fd86710b870
JTay92 <joe.r.tayler@outlook.com>
2023-04-11 18:56:40 +0100

    add function to check if value is valid

--- main.py
+++ main.py
@@ -12,6 +12,27 @@ board = [
     [0,0,0,0,0,0,9,0,7]
 ]
 
+def valid(bo, num, pos):
+    # Check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num  and pos[i] != 1:
+            return False
+        
+    # Check column
+    for i in range(len(bo[0])):
+        if bo[i][pos[1]] == num  and pos[0] != 1:
+            return False
+    
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+            
+    return True
 
 
 def draw_grid(bo):
